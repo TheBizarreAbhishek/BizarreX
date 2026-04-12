6f0c67577fa5a9eabe8a51b9b1f218e053643df6
Abhishek Babu <thebizarreabhishek@users.noreply.github.com>
2026-04-12 11:40:21 +0530

    Revert "Temp hack to delete duplicate folder"
    This reverts commit 0a39b7d2ba0a799595701bddc6249808955b220c.

--- tools/test_drive.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def test():
-    # 35mb video for testing, bypass limit is 100mb but this file triggers virus scan optionally
-    # Actually wait I need a 100mb+ public file from BizarreX to test.
-    pass
-
--- tools/test_drive_bypass.py
+++ tools/test_drive_bypass.py
@@ -1,44 +1,23 @@
 import requests
-import re
+import sys
 
-URL = "https://docs.google.com/uc?export=download&id=14yCWezECjIHvF7KQwsveA_KgVTLB38U1"
-session = requests.Session()
+def download_file_from_google_drive(id):
+    URL = "https://docs.google.com/uc?export=download&confirm=t"
+    session = requests.Session()
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+    print("Token: ", token)
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+    print("Response: ", response.status_code, response.headers.get('Content-Type'))
+    print("Length: ", response.headers.get('Content-Length'))
 
-# 1. First request
-res = session.get(URL, allow_redirects=False)
-print("1. docs.google.com status:", res.status_code)
-print("1. Location:", res.headers.get("Location"))
-print("1. Set-Cookie:", res.headers.get("Set-Cookie", ""))
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
 
-# Follow redirect manually
-next_url = res.headers.get("Location")
-if next_url:
-    res2 = session.get(next_url, allow_redirects=False)
-    print("2. drive.usercontent status:", res2.status_code)
-    print("2. Content-Type:", res2.headers.get("Content-Type"))
-    
-    if 'html' in str(res2.headers.get("Content-Type")):
-        html = res2.text
-        match = re.search(r'confirm=([a-zA-Z0-9_-]+)', html)
-        if match:
-            token = match.group(1)
-            print("2. Extracted token:", token)
-            
-            # Now retry with token using session cookies
-            final_url = URL + f"&confirm={token}"
-            print("3. Retrying URL:", final_url)
-            print("3. Sending Cookies:", session.cookies.get_dict())
-            
-            res3 = session.get(final_url, allow_redirects=False)
-            print("4. status:", res3.status_code)
-            print("4. Location:", res3.headers.get("Location"))
-            
-            if res3.status_code == 303:
-                res4 = session.get(res3.headers.get("Location"), allow_redirects=False)
-                print("5. FINAL status:", res4.status_code)
-                print("5. FINAL length:", res4.headers.get("Content-Length"))
-                print("5. FINAL Type:", res4.headers.get("Content-Type"))
-        else:
-            print("2. Token NOT FOUND in HTML")
-            print("HTML sample:", html[:500])
-
+if __name__ == "__main__":
+    download_file_from_google_drive(sys.argv[1])
